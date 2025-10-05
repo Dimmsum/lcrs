f1c573c4e405eb971e67dde61232bfc18395c1e0
Dimetri Lee <153735532+Dimmsum@users.noreply.github.com>
2025-10-05 13:14:30 -0500

    Delete config directory

--- config/config.py
+++
@@ -1,4 +0,0 @@
-RIOT_API_KEY = "RGAPI-2d3109a9-6639-4d54-b617-bb548432e1c2"
-REGION = "americas"                # Change to your region
-DATADRAGON_VERSION = "13.21.1"  # Update to latest from Riot Data Dragon
-MATCH_COUNT = 50              # Number of recent matches to analyze
